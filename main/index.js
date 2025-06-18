ff6fcdadb89748fce96b3eea1e262fb907885f36
= <=>
2025-06-18 18:00:07 +0700

    update semua jir

--- main/index.js
+++ main/index.js
@@ -1,39 +1,11 @@
-// let a = 8;
-// let b = 3;
-// let result1 = a ** b;
-// let result2 = b ** b;
-// let finish = result1 + result2;
-// console.log(finish);
-
-const volumeKubus = function (a, b) {
-  return a ** b + b ** b;
-};
-console.log(volumeKubus(8, 3));
-
-const kurang = function (a, b) {
-  return a - b;
-};
-console.log(kurang(78, 180));
-
-// const volumeKubus = (a, b) => a * b + a * b;
-// console.log(volumeKubus(10, 10));
-
-// function volumeKubus() {
-//   let result = 0;
-//   for (let i = 0; i < arguments.length; i++) {
-//     result += arguments[i];
+// const ar = function (a) {
+//   for (let i = 0; i < a.length; i++) {
+//     console.log(` Index ke-${i} : ${a[i]}`);
 //   }
-//   return result;
-// }
+// };
 
-// let example = volumeKubus(1, 2, 3);
-// console.log(example)
-// function name() {
-//   let total = 0;
-//   for (let i = 0; i < arguments.length; i++) {
-//     total += arguments[i];
-//   }
-//  return(total);
-// }
-// let example = name(1, 2, 3, 4);
-// console.log(example);
+// const ind = [1, 2, 3, 4, 5];
+// ar(ind);
+
+let arr = [1, 2, 3, 4, 5, [2, 4, 5, 6,]]
+console.log(arr[5][2])

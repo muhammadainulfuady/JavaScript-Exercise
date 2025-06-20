8ff8ea3071755d94be9e8d5a1635ef486f3a1b24
= <=>
2025-06-20 09:57:43 +0700

    update pembelajaran dengan object

--- main/index.js
+++ main/index.js
@@ -1,21 +1,6 @@
-let i = 0;
-let user = parseInt(prompt("Mau berapa kali loop ? "));
-let genap = [];
-let real = [];
+let dt_lst = ["saya", 20, 2.0, true, false];
 let total = 0;
-while (i < user) {
-  let user2 = parseInt(prompt(`Masukkan list ke-${i + 1}`));
-  real.push(user2);
-  if (user2 % 2 == 0) {
-    genap.push(user2);
-  }
-  total += user2;
-  i++;
+for (let i = 0; i < dt_lst.length; i++) {
+  console.log(`Data index ke-${i} adalah "${dt_lst[i]}"`);
+  total += i;
 }
-console.log("Array yang masuk:");
-console.log(real);
-console.log("\nAngka genap:");
-genap.forEach(function (angka) {
-  console.log(`${angka}`);
-});
-console.log(`Jumlah total \n${total}`);

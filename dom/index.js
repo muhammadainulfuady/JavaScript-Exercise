32e860e1b82189a033e67fe57cbc8e33ebfb0b66
= <=>
2025-09-03 12:44:26 +0700

    hmm

---
+++ dom/index.js
@@ -0,0 +1,25 @@
+// mengambil node body dari html
+const container = document.querySelector("body");
+
+// mengmabil id dari button untuk mengubah warna dari
+const tUbahWarna = document.getElementById("tombolUbahWarna");
+
+// event ketika suatu tombol itu di klik
+tUbahWarna.addEventListener("click", function () {
+  // reaksi tombol setelah di klik yaitu dia akan memberikan class biru-muda ke dalam container makanya dia bisa berubah warna
+  container.classList.toggle("biru-muda");
+});
+
+// membuat buutton
+const tAcakWarna = document.createElement("button");
+const teksTombol = document.createTextNode("Acak Warna");
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute("type", "button");
+tUbahWarna.after(tAcakWarna);
+
+tAcakWarna.addEventListener("click", function () {
+  const red = Math.round(Math.random() * 255 + 1);
+  const green = Math.round(Math.random() * 255 + 1);
+  const blue = Math.round(Math.random() * 255 + 1);
+  document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+});
--- main/index.js
+++ main/index.js
@@ -1,6 +1,68 @@
-let dt_lst = ["saya", 20, 2.0, true, false];
-let total = 0;
-for (let i = 0; i < dt_lst.length; i++) {
-  console.log(`Data index ke-${i} adalah "${dt_lst[i]}"`);
-  total += i;
+// function vod(name, umur, nilai) {
+//   return `Halo nama saya ${name}\nUmur saya ${umur} tahun\nNilai ujian saya ${nilai}`;
+// }
+// const nama = "Fuady";
+// const umur = 20;
+// const nilai = 75.5;
+// console.log(vod(nama, umur, nilai));
+
+// function luasPersegi(panjang, lebar) {
+//   return `Luas persegi panjang : ${
+//     panjang * lebar
+//   }\nKeliling persegi panjang : ${2 * (panjang + lebar)}`;
+// }
+// const luas1 = 16;
+// const luas2 = 13;
+// console.log(luasPersegi(luas1, luas2));
+
+// function nilaiUjian(nilai) {
+//   if (nilai >= 90) {
+//     console.log("Hasil akhir A");
+//   } else if (nilai >= 80) {
+//     console.log("Hasil akhir B");
+//   } else if (nilai >= 70) {
+//     console.log("Hasil akhir C");
+//   } else if (nilai >= 60) {
+//     console.log("Hasil akhir D");
+//   } else if (nilai < 60) {
+//     console.log("Hasil akhir E");
+//   }
+//   return `Nilai ujianmu ${nilai}`;
+// }
+
+// console.log(nilaiUjian(90));
+// function hitungHari(hari) {
+//   switch (hari) {
+//     case 1:
+//       console.log("senin");
+//       break;
+//     case 2:
+//       console.log("selasa");
+//       break;
+//     case 3:
+//       console.log("rabu");
+//       break;
+//     case 4:
+//       console.log("kamis");
+//       break;
+//     case 5:
+//       console.log("jumat");
+//       break;
+//     case 6:
+//       console.log("sabtu");
+//       break;
+//     case 7:
+//       console.log("minggu");
+//       break;
+//   }
+//   return hari;
+// }
+// hitungHari(2);
+
+function looping(nilai) {
+  for (let i = 1; i < nilai + 1; i++) {
+    console.log(i);
+  }
+  return nilai;
 }
+looping(10);
